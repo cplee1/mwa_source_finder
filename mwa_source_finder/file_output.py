ee6a54e523c01bd2148511f7a2e45e2e08cb3f61
cplee1 <c.p.lee@student.curtin.edu.au>
2024-02-29 17:26:30 +0800

    Improved file output and cleaned up plot function

--- mwa_source_finder/file_output.py
+++ mwa_source_finder/file_output.py
@@ -4,6 +4,7 @@ import numpy as np
 from astropy.table import Table
 import matplotlib.pyplot as plt
 import matplotlib.colors as mcolors
+from matplotlib import gridspec
 
 from mwa_source_finder import logger_setup
 
@@ -15,7 +16,9 @@ plt.rcParams["font.size"] = 12
 def write_output_source_files(
     finder_result: dict,
     obs_metadata_dict: dict,
-    min_z_power: float,
+    freq_mode: str,
+    norm_mode: str,
+    min_power: float,
     logger: logging.Logger = None,
 ):
     """Write finder results for each source.
@@ -26,8 +29,10 @@ def write_output_source_files(
         A dictionary containing the results organised by source.
     obs_metadata_dict : dict
         A dictionary of observation metadata dictionaries.
-    min_z_power: float
-        The minimum zenith-normalised power.
+    freq_mode: str
+        The frequency mode used ['low', 'centre', 'high'].
+    norm_mode: str
+        The beam normalisation mode used ['zenith', 'beam'].
     logger : logging.Logger, optional
         A custom logger to use, by default None.
     """
@@ -50,9 +55,9 @@ def write_output_source_files(
             dtype=[int, float, float, float, int, float, float],
             rows=beam_data_extended,
         )
-        data["Enter"].format = "%.2f"
-        data["Exit"].format = "%.2f"
-        data["Power"].format = "%.2f"
+        data["Enter"].format = "%.3f"
+        data["Exit"].format = "%.3f"
+        data["Power"].format = "%.3f"
         data["Freq"].format = "%.2f"
         data["BW"].format = "%.2f"
 
@@ -63,7 +68,10 @@ def write_output_source_files(
         divider_str = "# " + "-" * 78 + "\n"
         header = (
             divider_str
-            + f"# All observations where the source reaches a zenith normalised power >{min_z_power}\n"
+            + "# Source finder settings:\n"
+            + f"# Freq. mode -- {freq_mode}\n"
+            + f"# Norm. mode -- {norm_mode}\n"
+            + f"# Min power  -- {min_power:.2f}\n"
             + divider_str
             + "# Column headers:\n"
             + "# Obs ID -- Observation ID\n"
@@ -87,7 +95,11 @@ def write_output_source_files(
 def write_output_obs_files(
     finder_result: dict,
     obs_metadata_dict: dict,
-    min_z_power: float,
+    t_start: float,
+    t_end: float,
+    freq: float,
+    norm_mode: str,
+    min_power: float,
     logger: logging.Logger = None,
 ):
     """Write finder results for each observation.
@@ -98,6 +110,14 @@ def write_output_obs_files(
         A dictionary containing the results organised by observation ID.
     obs_metadata_dict : dict
         A dictionary of observation metadata dictionaries.
+    t_start: float
+        Start time of the search, as a fraction of the full observation.
+    t_end: float
+        End time of the search, as a fraction of the full observation.
+    freq: float
+        The frequency of the search.
+    norm_mode: str
+        The beam normalisation mode used ['zenith', 'beam'].
     logger : logging.Logger, optional
         A custom logger to use, by default None.
     """
@@ -119,9 +139,9 @@ def write_output_obs_files(
             ],
             rows=obs_data,
         )
-        data["Enter"].format = "%.2f"
-        data["Exit"].format = "%.2f"
-        data["Power"].format = "%.2f"
+        data["Enter"].format = "%.3f"
+        data["Exit"].format = "%.3f"
+        data["Power"].format = "%.3f"
 
         out_file = f"{obsid}_sources.txt"
         logger.info(f"Saving output file: {out_file}")
@@ -131,17 +151,24 @@ def write_output_obs_files(
         divider_str = "# " + "-" * 78 + "\n"
         header = (
             divider_str
-            + f"# All sources which reach a zenith normalised power >{min_z_power}\n"
+            + "# Observation metadata:\n"
             + f"# Obs ID      -- {obsid}\n"
-            + f"# Duration    -- {obs_metadata['duration']} s\n"
-            + f"# Centre freq -- {obs_metadata['centrefreq']} MHz\n"
-            + f"# Bandwidth   -- {obs_metadata['bandwidth']} MHz\n"
+            + f"# Centre freq -- {obs_metadata['centrefreq']:.2f} MHz\n"
+            + f"# Bandwidth   -- {obs_metadata['bandwidth']:.2f} MHz\n"
+            + f"# Duration    -- {obs_metadata['duration']:.0f} s\n"
+            + divider_str
+            + "# Source finder settings:\n"
+            + f"# Start time -- {t_start*obs_metadata['duration']:.2f} s\n"
+            + f"# End time   -- {t_end*obs_metadata['duration']:.2f} s\n"
+            + f"# Frequency  -- {freq/1e6:.2f} MHz\n"
+            + f"# Beam norm  -- {norm_mode}\n"
+            + f"# Min power  -- {min_power:.2f}\n"
             + divider_str
             + "# Column headers:\n"
-            + "# Obs ID -- Observation ID\n"
-            + "# Enter  -- The fraction of the observation when the source enters the beam\n"
-            + "# Exit   -- The fraction of the observation when the source exits the beam\n"
-            + "# Power  -- The maximum power of the source in the beam\n"
+            + "# Name  -- Source name\n"
+            + "# Enter -- The fraction of the time range when the source enters the beam\n"
+            + "# Exit  -- The fraction of the time range when the source exits the beam\n"
+            + "# Power -- The maximum beam power towards the source within the time range\n"
             + divider_str
             + "\n"
         )
@@ -153,133 +180,111 @@ def write_output_obs_files(
             f.writelines(lines)
 
 
+def setup_ticks(ax, fontsize=12):
+    ax.tick_params(axis="both", which="both", right=True, top=True)
+    ax.tick_params(axis="both", which="major", labelsize=fontsize, length=4)
+    ax.tick_params(axis="both", which="minor", length=2)
+    ax.tick_params(axis="both", which="both", direction="in")
+    ax.minorticks_on()
+
+
 def plot_power_vs_time(
     pointings: list,
     obs_metadata_dict: list,
     beam_coverage: dict,
-    obs_for_source: bool,
     min_power: float,
-    norm: bool,
     logger: logging.Logger = None,
 ):
     if logger is None:
         logger = logger_setup.get_logger()
 
     line_combos = []
-    for lsi in ["-", "--", "-.", ":"]:
+    for lsi in [
+        "-",
+        "--",
+        "-.",
+        ":",
+        (0, (5, 1, 1, 1, 1, 1)),
+        (0, (5, 1, 1, 1, 1, 1, 1, 1)),
+        (0, (5, 1, 5, 1, 1, 1)),
+        (0, (5, 1, 5, 1, 1, 1, 1, 1)),
+        (0, (5, 1, 5, 1, 5, 1, 1, 1)),
+        (0, (5, 1, 5, 1, 5, 1, 1, 1, 1, 1)),
+        (0, (5, 1, 1, 1, 5, 1, 5, 1, 1, 1)),
+        (0, (5, 1, 1, 1, 5, 1, 1, 1, 1, 1)),
+    ]:
         for coli in list(mcolors.TABLEAU_COLORS):
             line_combos.append([lsi, coli])
 
-    if obs_for_source:
-        # Plot of power in each observation, one plot per source
-        for pointing in pointings:
-            source_name = pointing["Name"]
-            fig, axes = plt.subplots(ncols=2, figsize=(10, 4), dpi=300)
-            ii = 0
-            max_duration = 0
-            for obsid in obs_metadata_dict:
-                if source_name in beam_coverage[obsid]:
-                    obs_duration = obs_metadata_dict[obsid]["duration"]
-                    if obs_duration > max_duration:
-                        max_duration = obs_duration
-                    _, _, _, source_power_TF = beam_coverage[obsid][source_name]
-                    source_power_FT = source_power_TF.T
-                    max_idx = np.unravel_index(
-                        np.argmax(source_power_FT), source_power_FT.shape
-                    )[0]
+    # Plot of power in each observation, one plot per source
+    for pointing in pointings:
+        source_name = pointing["Name"]
+        ii = 0
+        max_duration = 0
 
-                    # Plot powers
-                    times_sec = np.linspace(0, obs_duration, len(source_power_FT[0]))
-                    times_norm = np.linspace(0, 1, len(source_power_FT[0]))
-                    axes[0].errorbar(
-                        times_sec,
-                        source_power_FT[max_idx],
-                        ls=line_combos[ii][0],
-                        c=line_combos[ii][1],
-                        label=obsid,
-                    )
-                    axes[1].errorbar(
-                        times_norm,
-                        source_power_FT[max_idx],
-                        ls=line_combos[ii][0],
-                        c=line_combos[ii][1],
-                        label=obsid,
-                    )
-                    ii += 1
-            for ax in axes:
-                ax.fill_between(
-                    [0, max_duration],
-                    0,
-                    min_power,
-                    color="grey",
-                    alpha=0.3,
-                    hatch="///",
-                )
-                if norm:
-                    ax.set_ylim([0, 1])
-                else:
-                    ax.set_yscale("log")
+        fig = plt.figure(figsize=(8, 4), dpi=300)
+        gs = gridspec.GridSpec(1, 2, wspace=0.1)
+        ax0 = plt.subplot(gs[0])
+        ax1 = plt.subplot(gs[1])
+        axes = (ax0, ax1)
 
-            # Add legend
-            axes[1].legend(loc="center left", bbox_to_anchor=(1.1, 0.5))
-
-            # Set plot limits
-            axes[0].set_xlim([0, max_duration])
-            axes[1].set_xlim([0, 1])
-
-            # Set labels
-            fig.suptitle(source_name)
-            axes[0].set_xlabel("Time [s]")
-            axes[1].set_xlabel("Time [normalised]")
-            if norm:
-                axes[0].set_ylabel("Power [normalised]")
-            else:
-                axes[0].set_ylabel("Power")
-
-            # Save fig
-            if norm:
-                plot_name = f"power_vs_time_{source_name}.png"
-            else:
-                plot_name = f"power_vs_time_{source_name}_norm.png"
-            logger.info(f"Saving plot file: {plot_name}")
-            fig.savefig(plot_name, bbox_inches="tight")
-            fig.clf()
-
-    else:
-        # Plot of power for each source, one plot per observation
         for obsid in obs_metadata_dict:
-            obs_duration = obs_metadata_dict[obsid]["duration"]
-            fig, ax = plt.subplots(figsize=(5, 4), dpi=300)
-            for pointing in pointings:
-                source_name = pointing["Name"]
-                if source_name in beam_coverage[obsid]:
-                    _, _, _, source_power_TF = beam_coverage[obsid][source_name]
-                    source_power_FT = source_power_TF.T
-                    source_power_FT = source_power_TF.T
-                    max_idx = np.unravel_index(
-                        np.argmax(source_power_FT), source_power_FT.shape
-                    )[0]
-
-                    # Plot powers
-                    times_sec = np.linspace(0, obs_duration, len(source_power_FT[0]))
-                    ax.errorbar(
-                        times_sec,
-                        source_power_FT[max_idx],
-                        ls="-",
-                    )
+            if source_name in beam_coverage[obsid]:
+                obs_duration = obs_metadata_dict[obsid]["duration"]
+                if obs_duration > max_duration:
+                    max_duration = obs_duration
+                _, _, _, source_power = beam_coverage[obsid][source_name]
+                
+                # Plot powers
+                times_sec = np.linspace(0, obs_duration, len(source_power))
+                times_norm = np.linspace(0, 1, len(source_power))
+                axes[0].errorbar(
+                    times_sec,
+                    source_power,
+                    ls=line_combos[ii][0],
+                    c=line_combos[ii][1],
+                    label=obsid,
+                )
+                axes[1].errorbar(
+                    times_norm,
+                    source_power,
+                    ls=line_combos[ii][0],
+                    c=line_combos[ii][1],
+                    label=obsid,
+                )
+                ii += 1
+        for ax in axes:
             ax.fill_between(
-                [0, obs_duration], 0, min_power, color="grey", alpha=0.3, hatch="///"
+                [0, max_duration],
+                0,
+                min_power,
+                color="grey",
+                alpha=0.3,
+                hatch="///",
             )
-
-            # Set plot limits
             ax.set_ylim([0, 1])
-            ax.set_xlim([0, obs_duration])
+            setup_ticks(ax)
+        axes[0].set_xlim([0, max_duration])
+        axes[1].set_xlim([0, 1])
+        axes[1].set_yticklabels([])
+        axes[0].set_xlabel("Time [s]")
+        axes[1].set_xlabel("Time [normalised]")
+        axes[0].set_ylabel("Power [normalised]")
 
-            # Set labels
-            fig.suptitle(obsid)
-            ax.set_ylabel("Power [normalised]")
-            ax.set_xlabel("Time [s]")
+        handles, labels = axes[0].get_legend_handles_labels()
+        fig.legend(
+            handles,
+            labels,
+            loc="upper center",
+            bbox_to_anchor=(0.5, -0.02),
+            fancybox=True,
+            shadow=True,
+            ncol=3,
+        )
+        fig.suptitle(source_name)
 
-            # Save fig
-            fig.savefig(f"power_vs_time_{obsid}.png", bbox_inches="tight")
-            fig.clf()
+        # Save fig
+        plot_name = f"power_vs_time_{source_name}.png"
+        logger.info(f"Saving plot file: {plot_name}")
+        fig.savefig(plot_name, bbox_inches="tight")
+        fig.clf()
